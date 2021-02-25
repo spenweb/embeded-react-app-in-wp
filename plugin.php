33fff8c2acf517945890e2e063fa1a1b5b70067b
Spencer Brown <git@spencerwebservices.com>
2021-02-24 21:08:04 -0700

    Use alternative method of getting version number

--- plugin.php
+++ plugin.php
@@ -44,7 +44,7 @@ add_shortcode(
  */
 function md_react_app_enqueue_assets() {
 
-	$ver         = ( get_plugin_data( __FILE__ ) )['Version'];
+	$ver         = (get_file_data( __FILE__, ["Version" => "Version"], "plugin" ))['Version'];
 	$js_to_load  = plugin_dir_url( __FILE__ ) . 'app/build/static/js/main.js';
 	$css_to_load = plugin_dir_url( __FILE__ ) . 'app/build/static/css/main.css';
 
